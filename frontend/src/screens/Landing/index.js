82c51f1fb4b6f4739ebe329578897caf91d6a3b2
payalkherajani <payal.kherajani19@gmail.com>
2021-04-04 22:10:22 +0530

    frontend:landing-page-responsive

--- frontend/src/screens/Landing/index.js
+++ frontend/src/screens/Landing/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './landing.module.css';
-import { CategoryCard } from '../../components'
+import { CategoryCard } from '../../components';
+import windowImage from '../../assets/window.svg'
+import ReactPlayer from 'react-player'
 
 const Landing = () => {
+
     return (
         <div className={styles.landing_container}>
             <h1 className={styles.landing__heading}>Welcome</h1>
-            <div className={styles.landing__cards}>
-                <CategoryCard />
-                <CategoryCard />
-                <CategoryCard />
-                <CategoryCard />
+
+            <div className={styles.landing__grid}>
+
+                <div className={styles.landing__svg}>
+                    <h1>Welcome</h1>
+                    <img src={windowImage} alt='window-svg' className={styles.landing__window} />
+
+                    <ReactPlayer
+                        url='http://www.youtube.com/watch?v=YdAIBlPVe9s'
+                        width='100%'
+                        height='100%'
+                    />
+
+                </div>
+
+                <div className={styles.landing__cards}>
+                    <CategoryCard />
+                    <CategoryCard />
+                    <CategoryCard />
+                    <CategoryCard />
+                </div>
             </div>
+
         </div>
     )
 }
