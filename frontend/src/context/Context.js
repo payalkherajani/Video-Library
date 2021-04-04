6e6a61857edbc2264c677bfc783c5ec003b5ed40
payalkherajani <payal.kherajani19@gmail.com>
2021-04-04 16:24:23 +0530

    frontend:reducer-initial-setup

--- frontend/src/context/Context.js
+++ frontend/src/context/Context.js
@@ -1,11 +1,21 @@
-import { createContext } from 'react';
+import { createContext, useReducer } from 'react';
+import { reducer } from '../reducer/reducer';
 
 export const Context = createContext({});
 
+const initialState = {
+    videos: [],
+    playlist: [],
+    notes: [],
+    watchlater: [],
+    liked: []
+}
+
 export const Provider = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, initialState)
 
     return (
-        <Context.Provider value={{ name: 'Payal' }}>
+        <Context.Provider value={{ state, dispatch }}>
             { children}
         </Context.Provider>
     )
