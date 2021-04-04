6e6f3af56c03732c2b1abdf952f887e6c9921ec2
payalkherajani <payal.kherajani19@gmail.com>
2021-04-04 15:50:04 +0530

    frontend:basic-setup-done

---
+++ DataModel.js
@@ -0,0 +1,39 @@
+const VideoLibData = [
+    {
+        id: 1,
+        category: 'Books',
+        videos: ['id1', 'id2', 'id3', 'id4']
+    },
+
+    {
+        id: 2,
+        category: 'Education',
+        videos: ['id5', 'id6', 'id7', 'id8']
+    },
+
+    {
+        id: 3,
+        category: 'Entertainment',
+        videos: ['id9', 'id10', 'id11', 'id12']
+    },
+
+    {
+        id: 4,
+        category: 'Motivational',
+        videos: ['id13', 'id14', 'id15', 'id16']
+    },
+]
+
+
+// https://developers.google.com/youtube/v3/docs/videos/list
+
+// const initialState: {
+//     playlists: [ { id: '1',videos:['id1'...] } ],
+
+//     likedVideos: ['id1','id2'],
+
+//     savedVideos: ['id1','id2'],
+
+
+
+// }
