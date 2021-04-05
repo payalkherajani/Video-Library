02e8c5f9c262407b3d08d5b88a6c7af6f9000ddd
payalkherajani <payal.kherajani19@gmail.com>
2021-04-05 21:29:32 +0530

    frontend:transition-effect-added

--- frontend/src/components/categorycard/index.js
+++ frontend/src/components/categorycard/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import styles from './categorycard.module.css'
 
-const CategoryCard = () => {
+const CategoryCard = ({ channelname, image, channelId }) => {
     return (
         <div className={styles.categorycard_container}>
-            <img src="https://i.pinimg.com/originals/c7/e5/ae/c7e5ae44d75084fd6b5f253ce828e1d5.png" alt='categor-card' className={styles.categorycard__image} />
+            <img src={image} alt='categor-card' className={styles.categorycard__image} />
             <div className={styles.card__imageoverlay}>
-                Kara & Nate
+                {channelname}
             </div>
         </div>
     )
--- frontend/src/screens/Landing/index.js
+++ frontend/src/screens/Landing/index.js
@@ -25,10 +25,15 @@ const Landing = () => {
                 </div>
 
                 <div className={styles.landing__cards}>
-                    <CategoryCard />
-                    <CategoryCard />
-                    <CategoryCard />
-                    <CategoryCard />
+
+                    <CategoryCard image="https://i.pinimg.com/originals/c7/e5/ae/c7e5ae44d75084fd6b5f253ce828e1d5.png" channelname="Kara & Nate" channelId="UC4ijq8Cg-8zQKx8OH12dUSw" />
+
+                    <CategoryCard image="https://yt3.ggpht.com/ytc/AAUvwngopd1przlk3ZZDKxtw3hqN-Hr_BB8cs3qlAHuLoA=s900-c-k-c0x00ffffff-no-rj" channelname="Lexie Limitless" channelId="UCWoEpiHaC7LOQhaHFT8Rx7A" />
+
+                    <CategoryCard image="https://www.hotfridaytalks.com/wp-content/uploads/2019/02/3c73adf7-varun-vagish-640x480.jpg" channelname="MOUNTAIN TREKKER" channelId="UCl5dXugC3XZeDVsDkTaWJ4g" />
+
+                    <CategoryCard image="https://magazine.xpert.tv/wp-content/uploads/2020/05/unnamed-12.jpg" channelname="Tanya Khanijow" channelId="UCGeGhS_akOxBWQcSmje6B-w" />
+
                 </div>
             </div>
 
