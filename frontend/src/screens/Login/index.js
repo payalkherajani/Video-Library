bd1124ae2d2e05dfd44b2d9f44e7249266f9d65c
payalkherajani <payal.kherajani19@gmail.com>
2021-05-10 18:36:41 +0530

    frontend:otp-form-designed

--- frontend/src/screens/Login/index.js
+++ frontend/src/screens/Login/index.js
@@ -1,30 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './login.module.css';
 import { Link } from 'react-router-dom';
+import OtpInput from 'react-otp-input';
 
 const Login = () => {
+    const [show, setShow] = useState(false);
+
+    const onGenerateOTP = (e) => {
+        e.preventDefault();
+        setShow(true)
+    }
+
     return (
         <div className={styles.register_div}>
             <div className={styles.register_form}>
 
                 <div className={styles.form_div}>
-                    <form>
-                        <h1 className={styles.heading}>Login</h1>
+                    {
+                        show === false ? (
+                            <form onSubmit={onGenerateOTP} style={{ padding: "1rem" }}>
+                                <h1 className={styles.heading}>Login</h1>
 
-                        <div className={styles.form_container}>
-                            <input
-                                className={styles.input_login}
-                                placeholder="Email"
-                                type="email"
-                                name="email"
-                            // value={email}
-                            // onChange={handleFormData}
-                            />
-                        </div>
-                        <button className={`btn btn-primary ${styles.login_button}`}>Generate OTP</button>
-                        {/* <p>Note:  You may get OTP in spam folder of your mail</p> */}
-                        <p className={styles.check_status}> Don't have Account ? <Link to='/register' className={styles.text_white}>REGISTER</Link> </p>
-                    </form>
+                                <div className={styles.form_container}>
+                                    <input
+                                        className={styles.input_login}
+                                        placeholder="Email"
+                                        type="email"
+                                        name="email"
+                                    // value={email}
+                                    // onChange={handleFormData}
+                                    />
+                                </div>
+                                <button className={`btn btn-primary ${styles.login_button}`}>Generate OTP</button>
+                                {/* <p>Note:  You may get OTP in spam folder of your mail</p> */}
+                                <p className={styles.check_status}> Don't have Account ? <Link to='/register' className={styles.text_white}>REGISTER</Link> </p>
+                            </form>
+                        ) : (
+                            <form>
+                                <h1 className={styles.heading}>Login</h1>
+
+                                <div className={styles.form_container}>
+                                    <OtpInput
+                                        // value={this.state.otp}
+                                        // onChange={this.handleChange}
+                                        numInputs={6}
+                                        separator={<span>-</span>}
+                                        inputStyle={styles.inputStyle}
+                                    />
+                                </div>
+                                <button className={`btn btn-primary ${styles.login_button}`}>Login</button>
+                            </form>
+                        )
+                    }
+
                 </div>
 
                 <div className={styles.image}>
